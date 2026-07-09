54b9c9a7aa6eae06bf0547835b2de5cdfab0b6fa
itc-n26003 <n26003@std.it-college.ac.jp>
2026-07-09 01:36:54 +0000

    READMEのファイル名修正と第2章2.13を追加

--- chap02/q2_12_1.py
+++ chap02/q2_12_1.py
@@ -1,2 +1,6 @@
-from math import pow
+# import math
+# print(math.pow(2,3))
+
+# print(2**3)
+
 print(pow(2,3))
--- chap02/q2_12_2.py
+++ chap02/q2_12_2.py
@@ -1,5 +1,1 @@
-import random
-from random import randint
-
 print(random.randint(0,10))
-print(randint(0,10))
