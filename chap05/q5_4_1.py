c321dcf05e72c7c6c0acfc7a4520aa668235318c
itc-n26003 <n26003@std.it-college.ac.jp>
2026-07-22 01:04:14 +0000

    5-4 完了

---
+++ chap05/q5_4_1.py
@@ -0,0 +1,3 @@
+a,b,c,d,e = ('Sapporo', 'Sendai', 'Tokyo', 'Nagaya', 'Osaka')
+g = d + e
+print(g)
---
+++ chap05/q5_4_3.py
@@ -0,0 +1,9 @@
+#tp1 = (1,2,3,4,5)
+#tp2 = (1,2,3,4,5)
+#print([x + y for (x,y) in zip(tp1,tp2)])
+
+tp1 = (1,2,3,4,5)
+tp2 = (1,2,3,4,5)
+def tpadd(x,y):
+    return x + y
+print(list(map(tpadd,tp1,tp2)))
